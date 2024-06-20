da40ebeaa3d298041db88afe195de49cf6d296ed
nkerry <98847152+nkerry11@users.noreply.github.com>
2024-06-20 18:13:11 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,61 @@
+window.PorfolioConfig = {
+  name: 'nkerry11',
+  jobTitle: 'Full-stack Web Developer',
+  jobDescription: 'Cool developer. Getting things done right on the front-end side with passion and modesty.',
+  skills: [
+    {
+      name: 'JavaScript',
+      description: ' The tricky bit, making things work smoothly and cleanly.'
+    },
+    {
+      name: 'HTML',
+      description: ' Specifically HTML 5, the main code part of Full-Stack development.'
+    },
+    {
+      name: 'CSS',
+      description: ' Fluent to make the websites pop and look beautiful.'
+    }
+  ],
+  works: [
+    {
+      name: 'nkerry.online',
+      link: 'https://nkerry.online',
+      description: ' One of my main projects, and this website infact too!'
+    },
+    {
+      name: 'Coming Soon..',
+      link: 'https://theuselessweb.com/',
+      description: ' I have big plans, trust me. '
+    },
+    {
+      name: 'Coming Soon..',
+      link: 'https://theuselessweb.com/',
+      description: ' I have big plans, trust me. '
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Gaming',
+      description: ' Chill thing to do in your spare time eh?'
+    },
+    {
+      name: 'Coding',
+      description: ' New way to explore things and challange yourself.'
+    },
+    {
+      name: 'Rubick\'s Cube',
+      description: ' Weird little gismo.'
+    }
+  ],
+  contacts: {
+    email: 'MisterNatty11@gmail.com',
+    mobileNo: '',
+    phoneNo: '',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/nkerry11'
+      },
+    ]
+  }
+};
